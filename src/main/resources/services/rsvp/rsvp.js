2dae5addba0419edc69e4806359451b9e4c899ff
Bobby Westberg <bellfalasch@gmail.com>
2023-07-06 11:11:17 +0200

    wip: create data in repo

---
+++ src/main/resources/services/rsvp/rsvp.js
@@ -0,0 +1,28 @@
+const nodeLib = require('/lib/xp/node');
+// https://developer.enonic.com/docs/xp/stable/api/lib-node
+
+// Service documentation: https://developer.enonic.com/docs/xp/stable/runtime/engines/http-service
+// It responds here: http://localhost:8080/admin/site/preview/moviedb/draft/gem/test-event/_/service/com.gjensidige.internal.gem/rsvp
+
+const gemRepo = function () {
+    return nodeLib.connect({
+        repoId: "com.gjensidige.internal.gem",
+        branch: "master"
+    });
+};
+
+exports.get = function (req) {
+  const eventId = req.params.eventId || null;
+  var result = gemRepo.create({
+    _name: 'nerd',
+    displayName: 'Carpenter and IT expert',
+    likes: 'Plywood',
+    numberOfUselessGadgets: 123,
+    eventId: eventId
+  });
+  // TODO: store RSVP using the data you need, attach event ID to know which event.
+  // Name of person, timestamp, e-mail, attending/not attending, allergy info ... more? eventId
+
+  log.info(JSON.stringify(result,null,4));
+
+}
